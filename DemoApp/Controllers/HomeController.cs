af0a44681349f7c370832449e3a390b157936529
GherasimGeorgian <pe.casaro10@gmail.com>
2020-09-04 13:54:26 +0300

    Add project files.

---
+++ DemoApp/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Contact()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(string name,string desc)
+        {
+            ViewBag.Name = name;
+            ViewBag.Desc = desc;
+            return View();
+        }
+    }
+}
